170bb92b2217529824d107382db492a8dee5ff1b
Малова Наталия <nmalova@mail.ru>
2022-09-02 00:11:27 +0300

    Realise task7

---
+++ seminar_1/task7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напиши программу, которая принимает на входе число и на выходе показывакт последнюю цифру этого числа
+
+Console.WriteLine("Введите трехзначное число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000)
+{
+    Double count = number % 10;
+    Console.WriteLine(count);
+}
+else
+{
+    Console.WriteLine("Введено не трехзначное число");
+}
