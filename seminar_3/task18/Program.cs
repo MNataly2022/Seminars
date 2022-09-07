b8c3a290b24dfa423b3bcf099805bad892b4f87e
Малова Наталия <nmalova@mail.ru>
2022-09-07 13:30:33 +0300

    Realise task18

---
+++ seminar_3/task18/Program.cs
@@ -0,0 +1,27 @@
+﻿// 18: Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ if (number >= 1 && number <= 4)
+ {
+    if (number == 1)
+    {
+      Console.WriteLine("Диапазон координат: x>0, y>0"); 
+    }
+    else if (number == 2)
+    {
+      Console.WriteLine("Диапазон координат: x<0, y>0"); 
+    }
+    else if (number == 3)
+    {
+      Console.WriteLine("Диапазон координат: x<0, y<0"); 
+    }
+    else if (number == 4)
+    {
+      Console.WriteLine("Диапазон координат: x>0, y<0"); 
+    }
+ }
+else
+        Console.WriteLine("Существует только 4 координатных четверти"); 
