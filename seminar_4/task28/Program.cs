ed70ac320bd796d39bad3bee0d7d8e9d8c05bed6
Малова Наталия <nmalova@mail.ru>
2022-09-11 00:42:59 +0300

    .\seminar_1\Realize task28

---
+++ seminar_4/task28/Program.cs
@@ -0,0 +1,31 @@
+﻿// 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int ReadNumber(string massage)
+{
+Console.WriteLine(massage);                                        
+return Convert.ToInt32(Console.ReadLine()); 
+}
+
+int ProdNumberFrom1ToA(int N)
+{
+    if (N >= 1)
+      {
+         int sum = 1;
+         for (int i = 2; i <= N; i++)
+          {
+            sum = sum * i; 
+          }
+         return sum; 
+      }
+    else
+    {
+      Console.WriteLine("A- должно быть не меньше 1!");
+      return -1; 
+    }   
+}
+
+int number = ReadNumber("Введите число");
+int summa = ProdNumberFrom1ToA(number);
+Console.WriteLine($"Произведение от 1 до А = {summa}");
