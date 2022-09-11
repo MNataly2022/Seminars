c8cf35a108e4973be6389f48596f337f0df2d260
Малова Наталия <nmalova@mail.ru>
2022-09-11 22:55:03 +0300

    Realise task29

---
+++ Homework/task29/Program.cs
@@ -0,0 +1,13 @@
+﻿// 29: Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран. Данные вводятся с консоли пользователем
+
+
+ Console.WriteLine("Введите последовательно 8 элементов массива:  ");
+
+int[] array = new int[8]; 
+
+for(int i = 0; i < array.Length; i++)
+{
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(",", array) + "]");
