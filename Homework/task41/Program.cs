806a20c41dbf346bd78d5978d467738985a9d9ce
Малова Наталия <nmalova@mail.ru>
2022-09-22 00:06:30 +0300

    Realise task41

---
+++ Homework/task41/Program.cs
@@ -0,0 +1,32 @@
+﻿//  41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//      Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+//      0, 7, 8, -2, -2 -> 2   /   1, -7, 567, 89, 223-> 3
+
+// int ReadNumber(string massage)
+// {
+//     Console.WriteLine(massage);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+ Console.WriteLine("Введите последовательно числа:  ");
+
+int countOfPositive = 0;
+
+while (true)
+{
+    string answer = Console.ReadLine();
+    if (answer == "stop")
+    {
+        break;
+    }
+    else
+    {
+        int number = Convert.ToInt32(answer);
+        if (number > 0)
+        {
+            countOfPositive++;
+        }
+    }
+}
+Console.WriteLine($"Число положительных чисел: {countOfPositive}");
+
