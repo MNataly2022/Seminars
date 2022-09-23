6eca162789a63ec7f9215717884273c8d8638bfd
Малова Наталия <nmalova@mail.ru>
2022-09-23 22:40:10 +0300

    .\Homework\Realize task50

---
+++ Homework/task50/Program.cs
@@ -0,0 +1,64 @@
+﻿// 50. Напишите программу, которая на вход принимает позиции элемента
+//  в двумерном массиве, и возвращает значение этого элемента или же указание, 
+//  что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1 7 -> такого числа в массиве нет
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+
+int x = ReadNumber("Введите индекс элемента по строке");
+int y = ReadNumber("Введите индекс элемента по столбцу");
+
+if (x < 0 || y < 0)
+{
+    Console.WriteLine("Вы ввели отрицательный индекс");
+}
+else if (x > m || y > n)
+{
+    Console.WriteLine("ВЫ ввели индекс за пределами массива");
+}
+else
+{
+    Console.WriteLine($"По указанным индексам найден элемент, равный {matr[x, y]} ");
+}
