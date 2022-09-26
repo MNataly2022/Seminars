ce038962ede6a9659b6ea5d5133776dceacf950d
Малова Наталия <nmalova@mail.ru>
2022-09-26 12:47:31 +0300

    Realise task57

---
+++ seminar_8/task57/Program.cs
@@ -0,0 +1,73 @@
+﻿// 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+// 1, 2, 3    1 встречается 3 раза   4 встречается 1 раз
+// 4, 6, 1    2 встречается 2 раз    6 встречается 2 раза
+// 2, 1, 6    3 встречается 1 раз
+// В нашей исходной матрице встречаются элементы от 0 до 9
+
+int[,] GetMatrix(int rowsCount, int columnsCount, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matrix = new int[rowsCount, columnsCount];
+
+    Random rand = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+
+    return matrix;
+}
+
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void CountNumbers(int[,] matrix)
+{
+    int count = 0;
+    for (int k = 0; k < 10; k++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] == k)
+                {
+                  count++;
+                }
+             }
+        }     
+    if(count > 0)
+    {
+        Console.WriteLine($"{k} встречается {count} раз(а)");
+        count = 0;
+    } 
+    }
+}
+
+int m = ReadNumber("Введите количество строк");
+int n = ReadNumber("Введите количество столбцов");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine("");
+CountNumbers(matr);
